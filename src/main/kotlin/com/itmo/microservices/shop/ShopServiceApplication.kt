815b6885643c4940e7df6c2a570a2bb2db66bfdf
Nikita Baranov <baranovnikita18@gmail.com>
2022-03-28 20:36:55 +0300

    fix

--- src/main/kotlin/com/itmo/microservices/shop/ShopServiceApplication.kt
+++ src/main/kotlin/com/itmo/microservices/shop/ShopServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ShopServiceApplication
+class DemoServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<ShopServiceApplication>(*args)
+    runApplication<DemoServiceApplication>(*args)
 }
